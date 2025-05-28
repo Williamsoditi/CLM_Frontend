181051c984e8abb9c6785afe917a7d47b04cd73a
Williamsoditi <williamsoditi99@gmail.com>
2025-05-28 14:56:51 +0300

    Feat : Adds navbar logic to be completed tomorrow

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,43 @@
+import logo from '../assets/logo.png'
+import { NavBarMenu } from '../MockData/NavData';
 
 const NavBar = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <nav className='flex justify-between items-center py-3'>
+        {/* LOGO SECTION */}
+        <div className="container flex justify-between items-center py-3">
+          <a href="/">
+            <div className="text-1xl flex items-center font-bold">
+              <img
+                src={logo}
+                alt="logo"
+                className="navbar-logo h-20 w-auto max-w-full rounded-md object-contain"
+              />
+              <p>Clique Mambas</p>
+            </div>
+          </a>
+        </div>
 
-export default NavBar
+        {/* MENU SECTION */}
+        <div className='hidden lg:block'>
+          <ul>
+            {
+              NavBarMenu.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <a href="{item.link}" className='inline-block text-black'>{item.title}</a>
+                  </li>
+                );
+              })
+              
+            }
+          </ul>
+        </div>
+
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
